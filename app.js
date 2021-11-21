454b02b4e466e6177be1c2af21ca1e3d1d954d3c
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2021-11-21 09:53:42 +0530

    Adding income or expense item to UI

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var budgetController=(function(){
 
-    var Expense=(id,description,value)=>{
+    var Expense=function(id,description,value){
         this.id=id;this.description=description;this.value=value
 
     }
-    var Income=(id,description,value)=>{
+    var Income= function(id,description,value){
         this.id=id;this.description=description;this.value=value
-
     }
 
     let data={
@@ -17,6 +16,27 @@ var budgetController=(function(){
         totals:{
             exp:0,
             inc:0
+        }
+    }
+
+    return {
+        addItem:(type,des,val)=>{
+            let newItem,Id;
+            //Create Id
+            if(data.allItems[type].length>0)
+            Id=data.allItems[type][data.allItems[type].length-1].id+1;
+            else
+            Id=0;
+            if(type==='inc')
+            newItem=new Income(Id,des,val);
+            else{
+                newItem=new Expense(Id,des,val);
+            }
+            //push into inc or exp array
+            data.allItems[type].push(newItem);
+            console.log(newItem);
+            return newItem;
+
         }
     }
     
@@ -36,6 +56,47 @@ var UIController=(function(){
                 value:value.value,
                 type
             }
+
+        },
+        addListItem:(obj,type)=>{
+            var html,newHtml;
+           if(type==='inc')
+           {
+               html= `<div class="item clearfix" id="income-%id%">
+               <div class="item__description">%description%</div>
+               <div class="right clearfix">
+                   <div class="item__value">%value%</div>
+                   <div class="item__delete">
+                       <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                   </div>
+               </div>
+           </div>`
+           }
+           else if(type==='exp')
+           {
+               html=`<div class="item clearfix" id="expense-%id%">
+               <div class="item__description">%description%</div>
+               <div class="right clearfix">
+                   <div class="item__value">%value%</div>
+                   <div class="item__percentage">21%</div>
+                   <div class="item__delete">
+                       <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                   </div>
+               </div>
+           </div>`
+           }
+           console.log(html);
+           
+           newHtml=html.replace('%id%',obj.id);
+           newHtml=newHtml.replace('%description%',obj.description);
+           newHtml=newHtml.replace('%value%',obj.value);
+           console.log(newHtml);
+           var explist=document.querySelector('.expenses__list');
+           var inclist=document.querySelector('.income__list');
+           if(type==='inc')
+           inclist.insertAdjacentHTML('beforeend',newHtml);
+
+          
 
         }
     }
@@ -57,10 +118,10 @@ var controller=(function(budgetCtrl, UICtrl){
     }
     var ctrlAddItem=function(){
         
-       const inpdata= UICtrl.getInput();
-       console.log(inpdata.description);
-       console.log(inpdata.value);
-       console.log(inpdata.type);
+       var inpdata= UICtrl.getInput();
+       var newItem=budgetCtrl.addItem(inpdata.type,inpdata.description,inpdata.value);
+       UICtrl.addListItem(newItem,inpdata.type);
+       
         
         
     }
